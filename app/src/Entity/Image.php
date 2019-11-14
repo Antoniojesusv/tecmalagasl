bf952c10c441a0177889326c328aa83980fdf2d4
Antonio Jesús Vázquez Muñoz <antoniojesusvazquezmunozz@gmail.com>
2019-11-14 20:01:49 +0100

    feat: implemented entities

---
+++ app/src/Entity/Image.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\ImageRepository")
+ */
+class Image
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $uri;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getUri(): ?string
+    {
+        return $this->uri;
+    }
+
+    public function setUri(string $uri): self
+    {
+        $this->uri = $uri;
+
+        return $this;
+    }
+}
---
+++ app/src/Entity/Product.php
@@ -0,0 +1,112 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\ProductRepository")
+ */
+class Product
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $name;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $price;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $description;
+
+    /**
+     * @ORM\ManyToMany(targetEntity="App\Entity\Characteristic", inversedBy="products")
+     */
+    private $characteristic;
+
+    public function __construct()
+    {
+        $this->characteristic = new ArrayCollection();
+    }
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): self
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getPrice(): ?int
+    {
+        return $this->price;
+    }
+
+    public function setPrice(int $price): self
+    {
+        $this->price = $price;
+
+        return $this;
+    }
+
+    public function getDescription(): ?string
+    {
+        return $this->description;
+    }
+
+    public function setDescription(string $description): self
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection|Characteristic[]
+     */
+    public function getCharacteristic(): Collection
+    {
+        return $this->characteristic;
+    }
+
+    public function addCharacteristic(Characteristic $characteristic): self
+    {
+        if (!$this->characteristic->contains($characteristic)) {
+            $this->characteristic[] = $characteristic;
+        }
+
+        return $this;
+    }
+
+    public function removeCharacteristic(Characteristic $characteristic): self
+    {
+        if ($this->characteristic->contains($characteristic)) {
+            $this->characteristic->removeElement($characteristic);
+        }
+
+        return $this;
+    }
+}
