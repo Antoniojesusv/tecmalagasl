ad1d19b805a0ae5fc07489ba3215864db97c905f
Antonio Jesús Vázquez Muñoz <antoniojesusvazquezmunozz@gmail.com>
2019-11-28 20:22:09 +0100

    feat: imported and implemented general controller

--- app/assets/js/app.js
+++ app/assets/js/app.js
@@ -9,6 +9,8 @@
 import { TextController } from './controller/textController';
 import { HttpServiceAyax } from './service/ajaxService';
 
+import { GeneralController } from './controller/generalController';
+
 require('../css/app.css');
 
 require('@fortawesome/fontawesome-free/js/all.js');
@@ -30,10 +32,52 @@ $('#menu-toggle').click(function(e) {
 
 console.log('Hello Webpack Encore! Edit me in assets/js/app.js');
 
+const pathName = window.location.pathname;
+
+console.log(pathName);
+
 window.addEventListener('load', event => {
     const pathName = window.location.pathname;
-    if (pathName === '/text/') {
-        const httpServiceAyax = new HttpServiceAyax();
+    const httpServiceAyax = new HttpServiceAyax();
+
+    if (pathName === '/admin/text/') {
         new TextController({ httpServiceAyax });
+    }
+
+    if (pathName === '/') {
+        console.log('cargo controlador inicial');
+
+        const prev = window.document.getElementById('prev');
+        const next = window.document.getElementById('next');
+
+        let slideIndex = 1;
+
+        const showSlides = function(n) {
+            const slides = document.getElementsByClassName('mySlides');
+            if (n > slides.length) {
+                slideIndex = 1;
+            }
+            if (n < 1) {
+                slideIndex = slides.length;
+            }
+            for (let i = 0; i < slides.length; i++) {
+                slides[i].style.display = 'none';
+            }
+            slides[slideIndex - 1].style.display = 'flex';
+        };
+
+        showSlides(slideIndex);
+
+        next.addEventListener('click', () => {
+            showSlides((slideIndex += 1));
+        });
+
+        prev.addEventListener('click', () => {
+            showSlides((slideIndex += -1));
+        });
+    }
+
+    if (pathName !== '/' && pathName !== '/admin/text/') {
+        new GeneralController({ httpServiceAyax });
     }
 });
